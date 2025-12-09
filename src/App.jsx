7d6160033c872b388b181a3e232b0ddb65620a8e
Michael Bamidele <michaelbamidele12345@gmail.com>
2025-12-09 10:40:53 +0100

    Initial project files

---
+++ src/App.jsx
@@ -0,0 +1,189 @@
+import { CiTwitter } from "react-icons/ci";
+import { BsGithub } from "react-icons/bs";
+import { CiLinkedin } from "react-icons/ci";
+
+import { FaMapLocation } from "react-icons/fa6";
+
+
+
+
+
+
+function App() {
+ 
+  return (
+ <div>
+
+
+
+  <div className='bg-black w-full fixed flex justify-center pb-4 pt-4'>
+        <div className='flex text-white mt-3 mb-4 text-2xl font-bold '>
+
+          <a href='https://www.twitter.com/tobithegldnboy' target='_' className=" hover:text-zinc-500 text-white mr-4"> <CiTwitter />
+          </a>
+
+          <a href='https://github.com/dev-mike3' target='_' className=" hover:text-zinc-500 text-white mr-4"> <BsGithub />
+          </a>
+
+          <a href='https://www.linkedin.com/in/michael-bamidele-a31865275/' target='_' className=" hover:text-zinc-500 text-white mr-4"> <CiLinkedin />
+          </a>
+        </div>
+      </div>
+
+      <div data-v-747b8a2a="" class="pt-20 px-10 pb-5 bg-black">
+        <img data-v-747b8a2a="" class="w-32 h-32 rounded-full" src="src/assets/blackman.jpg" alt="my image" />
+
+        <div className='flex pt-4' >
+          <i class=" text-gray-300 mr-2 ml-1">
+            <FaMapLocation />
+          </i>
+
+          <h3 class="text-mono text-white text-xs "> Nigeria</h3>
+
+        </div>
+
+        <div class="w-50 bg-black border border-t-gray-400 border-b-gray-400 border-r-black border-l-black h-8 mt-5 mx-0">
+          <a class="font-extrabold mt-1 mb-1 text-sm text-white .wave-emoji" href="https://www.dropbox.com/scl/fi/znla2dfnhw451fbxyxacm/My-Resume.docx?rlkey=4m9rylg0yx73ujnepe5vl0aog&st=ub6z3rtd&dl=0" download="" target='_' data-v-747b8a2a=""> → RESUME </a>
+
+        </div>
+
+        <div data-v-0f153a25="" class="px-5 py-10 bg-black pl-10"><div data-v-0f153a25="" class="text-gray-400 text-sm"><h3 data-v-0f153a25="" class="font-extrabold text-xl text-gray-100 .wave-emoji">Hi, I'm Michael 👋</h3>
+          <h3 data-v-0f153a25="">I am a Fullstack developer with the zeal for <span data-v-0f153a25="" class="text-blue-400"> technology </span> and solving problems. I am a punctual and well disciplined individual with love for excellence. I am determined to create  <span data-v-0f153a25="" class="text-blue-400">World class</span>, sites and projects for companies, individulas and startups.The tech scene worldwide is growing and my job is to take part of it and contribute diligently.</h3></div></div>
+
+        <div data-v-a04902c4="" class="bg-black py-5 px-5 pl-10 lg:px-10"><h3 class="font-extrabold text-xl text-gray-100 .wave-emoji pt-3" data-v-a04902c4="">My Tools </h3><br data-v-a04902c4="" /><br data-v-a04902c4="" /><div class="flex flex-wrap justify-between" data-v-a04902c4="">
+
+          <img class=" text-white text-5xl mx-2 lg:mr-4 my-2 lg:mx-4 hover:text-gray-500 w-14" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/supabase/supabase-original.svg" />
+    
+
+
+          
+            <img class=" text-white text-5xl mx-2 lg:mr-4 my-2 lg:mx-4 hover:text-gray-500 w-14"src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" />
+          
+          <img class=" text-white text-5xl mx-2 lg:mr-4 my-2 lg:mx-4 hover:text-gray-500 w-14" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" />
+          <img class=" text-white text-5xl mx-2 lg:mr-4 my-2 lg:mx-4 hover:text-gray-500 w-14" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+          <img class=" text-white text-5xl mx-2 lg:mr-4 my-2 lg:mx-4 hover:text-gray-500 w-14" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
+
+        </div>
+
+
+          <div class="bg-black">
+            <h3 class="font-extrabold text-xl text-gray-100 .wave-emoji  py-3"> Projects 📁</h3>
+
+            <div class="bg-black">
+            <h3 class="text-gray-400 text-sm  mr-8"><span class="text-bg-blue">I build stuffs I want to use</span> , as a software engineer, I spend my free time immersed in learning new technologies and honing my skills, constantly exploring innovative ways to build and create remarkable solutions. It's a fulfilling journey where passion meets knowledge, and <span class="text-blue-400">thrive in the realm of endless possibilities.</span><br /></h3><h2 class="text-xl  mt-3 text-gray-300"> To mention a few →</h2>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+
+
+
+      <div>
+
+
+
+      </div>
+      <div class="md:flex md:items-center py-10 md:flex-col bg-black sm:flex-col lg:flex-row lg:justify-center lg:flex-wrap lg:flex">
+        
+        <a href="https://todo-list-one-beta.vercel.app/">
+          
+          <div class="h-96 sm:ml-2 border-b border-l border-r border-gray-800 rounded-lg w-100 sm:w-72 pb-2">
+            <img class="h-72 sm:h-48 rounded-t-9xl mb-3" src="/task.png"/><h3 class="sm:text-sm sm:ml-2 text-bold font-pop font-bold lg:ml-8 text-white"> Task Manager - An app to boost productivity  <br/><br/>Technologies<br/><h2 class="text-sm">Frontend → HTML,CSS + Javascript <br/> Framework → React </h2></h3></div>
+            
+            </a>
+
+            
+            <br/>
+            <br/>
+
+
+
+            <a href="https://ngbuka-blog-forum.vercel.app/">
+          
+          <div class="h-96 sm:ml-2 border-b border-l border-r border-gray-800 rounded-lg w-100 sm:w-72 pb-2">
+            <img class="h-72 sm:h-48 rounded-t-9xl mb-3" src="/blog.png"/><h3 class="sm:text-sm sm:ml-2 text-bold font-pop font-bold lg:ml-8 text-white">Interactive Automobile Forum <br/><br/>Technologies<br/><h2 class="text-sm">Frontend → HTML,CSS + Javascript. Backend → Node.js <br/> Framework → Next.js </h2></h3></div></a>
+
+            <br/>
+            <br/>
+
+            
+
+            <a href="https://orbithost.vercel.app/">
+          
+          <div class="h-96 sm:ml-2 border-b border-l border-r border-gray-800 rounded-lg w-100 sm:w-72 pb-2">
+            <img class="h-72 sm:h-48 rounded-t-9xl mb-3" src="/orbit.png"/> <h3 class="sm:text-sm sm:ml-2 text-bold font-pop font-bold lg:ml-8 text-white"> OrbitHost- A Demo of a scalable webhosting platform <br/><br/>Technologies<br/><h2 class="text-sm">Frontend → HTML,CSS + Javascript <br/> Framework → React </h2></h3></div></a>
+
+            <br/>
+            <br/>
+            
+
+            
+
+            <a href="https://random-password-sigma.vercel.app/">
+          
+          <div class="h-96 sm:ml-2 border-b border-l border-r border-gray-800 rounded-lg w-100 sm:w-72 pb-2">
+            <img class="h-72 sm:h-48 rounded-t-9xl mb-3" src="/passgen.png"/><h3 class="sm:text-sm sm:ml-2 text-bold font-pop font-bold lg:ml-8 text-white"> Password Generator - Creates strong passwords for users randomly<br/><br/>Technologies<br/><h2 class="text-sm">Frontend → HTML,CSS + Javascript <br/> Framework → React </h2></h3></div></a>
+
+            
+            <br/>
+            <br/>
+            
+
+            
+
+            <a href="https://random-color-sepia.vercel.app/">
+          
+          <div class="h-96 sm:ml-2 border-b border-l border-r border-gray-800 rounded-lg w-100 sm:w-72 pb-2">
+            <img class="h-72 sm:h-48 rounded-t-9xl mb-3" src="colorgen.png"/><h3 class="sm:text-sm sm:ml-2 text-bold font-pop font-bold lg:ml-8 text-white"> RGB Colour Generator<br/><br/>Technologies<br/><h2 class="text-sm">Frontend → HTML,CSS + Javascript <br/> Framework → React </h2></h3></div></a>
+
+            
+        
+            <br/>
+            <br/>
+
+
+
+            <a href="https://jumia-clone-kappa.vercel.app/">
+          
+          <div class="h-96 sm:ml-2 border-b border-l border-r border-gray-800 rounded-lg w-100 sm:w-72 pb-2">
+            <img class="h-72 sm:h-48 rounded-t-9xl mb-3" src="/jumia.png"/><h3 class="sm:text-sm sm:ml-2 text-bold font-pop font-bold lg:ml-8 text-white"> Jumia Clone<br/><br/>Technologies<br/><h2 class="text-sm">Frontend → HTML,CSS + Javascript <br/> Framework → React </h2></h3></div></a>
+            
+
+
+            <br/>
+            <br/>
+
+
+            <a href="https://scientific-calculator-omega.vercel.app/">
+            
+          <div class="h-96 sm:ml-2 border-b border-l border-r border-gray-800 rounded-lg w-100 sm:w-72 pb-2">
+            <img class="h-72 sm:h-48 rounded-t-9xl mb-3" src="/file.png"/><h3 class="sm:text-sm sm:ml-2 text-bold font-pop font-bold lg:ml-8 text-white">File Sharing Application<br/><br/>Technolgies<br/><h2 class="text-sm">Frontend → HTML,CSS + Javascript Backend→ Cloudinary+Supabase <br/> Framework → React </h2></h3></div></a>
+            
+
+            <br/>
+            <br/>
+
+            <a href="https://image-slider-mt5a.vercel.app/">
+           
+          
+          <div class="h-96 sm:ml-2 border-b border-l border-r border-gray-800 rounded-lg w-100 sm:w-72 pb-2">
+            <img class="h-72 sm:h-48 rounded-t-9xl mb-3" src="/gallery_demo.png"/><h3 class="sm:text-sm sm:ml-2 text-bold font-pop font-bold lg:ml-8 text-white"> Image Slider<br/><br/>Technologies<br/><h2 class="text-sm">Frontend → HTML,CSS + Javascript <br/> Framework → React </h2></h3></div></a>
+
+
+            <div class="flex justify-center bg-black border-t-2 pt-8 pb-8 border-t-gray-800 lg:w-full"><h3 class="font-pop text-sm text-white">© 2025 Dev Mike. </h3></div>
+
+            
+            
+</div>
+
+
+ </div>
+  )
+}
+
+export default App;
+
+
+// git remote add origin https://github.com/dev-mike3/My-Portfolio.git
